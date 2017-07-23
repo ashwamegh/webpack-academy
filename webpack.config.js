81507cfa40b7285e67de3aa547caf2bc828416af
shashank7200 <shashank7200@gmail.com>
2017-07-23 19:40:47 +0530

    created rules and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,24 @@ module.exports ={
     output: {
         filename: "bundle.js",
         path    : path.join(__dirname,'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use : "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use : [
+                    {
+                       loader: "style-loader"
+                    },
+                    {
+                       loader: "css-loader"
+                    }
+                ]
+            }
+        ]
     }
 }
