4be8ca8ca905e6da1f5c0f098dee59f67905075b
shashank7200 <shashank7200@gmail.com>
2017-08-02 00:41:00 +0530

    making webpack manual functions to work

--- webpack: Web Fundamentals/webpack.config.js
+++ webpack: Web Fundamentals/webpack.config.js
@@ -1,39 +1,50 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports ={
-    entry: "./src/index.js",
-    output: {
-        filename: "bundle.js",
-        path    : path.join(__dirname,'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jpe?g$/,
-                use : [
-                    "file-loader"
-                ]
-            }
-            // {
-            //     test: /\.js$/,
-            //     use : "babel-loader"
-            // },
-            // {
-            //     test: /\.css$/,
-            //     use : [
-            //         {
-            //            loader: "style-loader"
-            //         },
-            //         {
-            //            loader: "css-loader"
-            //         }
-            //     ]
-            // }
-        ],
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin(),
-        new webpack.ContextReplacementPlugin()
-    ]
+module.exports = (env) => {
+    console.log(env);
+    return {
+        entry: "./src/index.js",
+        output: {
+            filename: "bundle.js",
+            path    : path.join(__dirname,'dist')
+        }
+    }
 }
+
+// module.exports ={
+//     entry: "./src/index.js",
+//     output: {
+//         filename: "bundle.js",
+//         path    : path.join(__dirname,'dist')
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.jpe?g$/,
+//                 use : [
+//                     "file-loader"
+//                 ]
+//             }
+//             {
+//                 test: /\.js$/,
+//                 use : "babel-loader"
+//             },
+//             {
+//                 test: /\.css$/,
+//                 use : [
+//                     {
+//                        loader: "style-loader"
+//                     },
+//                     {
+//                        loader: "css-loader"
+//                     }
+//                 ]
+//             }
+//         ],
+//     },
+//     plugins: [
+//         new webpack.optimize.UglifyJsPlugin(),
+//         new webpack.ContextReplacementPlugin()
+//     ]
+// }
